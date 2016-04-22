4fb02deb0da8cb49d37ba10e7623a8ce9d0e9708
PangMing <1132700944@student.mmu.edu.my>
2016-04-22 14:46:54 +0800

    Update combine_img.py
    http://bryceboe.com/2010/05/25/defcon-18-quals-forensics-200-write-up/

--- combine_img.py
+++ combine_img.py
@@ -1,6 +1,19 @@
-sd
-v
+#!/usr/bin/env python
+import Image
 
-
-
+def main():
+  new = Image.open('IMG_0001.png')
+  w, h = new.size
+  for i int range(2, 1122):
+    im = Image.open('IMG_%04d.png' % i)
+    data= im.split()
+    for pixel, value in enumerate(im.getdata()):
+      if value[3] !=0: # not transparent
+        x, y = (pixel % w, pixel * 1. /w)
+        new.putpixel((x,y),value)
+  new.save('f200_result.png')
+  new.shot()
+  
+if __name__ == '__main__':
+  main()
 
